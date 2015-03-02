68db1263eb8aed0b8ddb25ab06d01a0a80dba864
Muhammad92 <saadmehmood21@gmail.com>
2015-03-03 01:22:48 +0500

    Wrapper for JSON FORESQUARE

---
+++ TestingApp/Models/NearyByJson.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingApp.Models
+{
+    public class NearyByJson
+    {
+        public List<vene> venes { get; set; }
+    }
+    public class vene 
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public string country { get; set; }
+        public string city { get; set; }
+        public string phone { get; set; }
+        public int distance { get; set; }
+
+        public double lat { get; set; }
+        public double lng { get; set; }
+        public int checkinsCount { get; set; }
+    }
+
+
+}
