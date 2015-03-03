0f2d899e894806ddf6f23c1c104f3593260f2bbd
Muhammad92 <saadmehmood21@gmail.com>
2015-03-03 14:15:10 +0500

    Addition of Models and View Models

---
+++ TestingApp/Models/RootStore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingApp.Models
+{
+    public class Item
+    {
+        public string name { get; set; }
+        public string stars { get; set; }
+        public string health { get; set; }
+        public string tag { get; set; }
+        public string image { get; set; }
+    }
+
+    public class RootStore
+    {
+        public List<Item> items { get; set; }
+    }
+}
---
+++ TestingApp/ViewModels/StorePageView Model.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TestingApp.Models;
+
+namespace TestingApp.ViewModels
+{
+     public class StorePageView_Model
+    {
+         public ObservableCollection<Item> obs_Store { get; set; }
+        public StorePageView_Model() 
+        {
+              obs_Store = new ObservableCollection<Item>();
+              GetJson();
+        }
+
+
+        public void GetJson() 
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadStringAsync(new Uri("http://hello987.azurewebsites.net/store.html"));
+            webClient.DownloadStringCompleted += webClient_DownloadStringCompleted;
+          
+        }
+        void webClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            var rootObject = JsonConvert.DeserializeObject<RootStore>(e.Result);
+            foreach (Item everyItemInList in rootObject.items) 
+            {
+                obs_Store.Add(everyItemInList);
+            }
+        }
+    }
+}
