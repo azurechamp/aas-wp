7a159db6de00d76d2a63f743e6cf4212361d68c2
Muhammad92 <saadmehmood21@gmail.com>
2015-03-30 16:08:36 +0500

    Added , Exercise Changed Main Hub Added Articles

--- TestingApp/Views/ExerciseSession.xaml.cs
+++ TestingApp/Views/ExerciseSession.xaml.cs
@@ -7,14 +7,94 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.Device.Location;
+using Microsoft.Phone.Maps.Controls;
+using System.Windows.Threading;
+using System.Windows.Media;
 
 namespace TestingApp.Views
 {
     public partial class ExerciseSession : PhoneApplicationPage
     {
+        //
+        private double _kilometres;
+        private long _previousPositionChangeTick;
+        private GeoCoordinateWatcher _watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+        private MapPolyline _line;
+        private DispatcherTimer _timer = new DispatcherTimer();
+        private long _startTime;
+        //
+
+
+
         public ExerciseSession()
         {
             InitializeComponent();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += Timer_Tick;
+
+
+
+            // create a line which illustrates the run
+            _line = new MapPolyline();
+            _line.StrokeColor = Colors.Red;
+            _line.StrokeThickness = 5;
+            Map.MapElements.Add(_line);
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+            _watcher.Start();
+            _watcher.PositionChanged += Watcher_PositionChanged;
+        }
+
+
+        private void Watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            var coord = new GeoCoordinate(e.Position.Location.Latitude, e.Position.Location.Longitude);
+
+            if (_line.Path.Count > 0)
+            {
+                // find the previos point and measure the distance travelled
+                var previousPoint = _line.Path.Last();
+                var distance = coord.GetDistanceTo(previousPoint);
+
+                // compute pace
+                var millisPerKilometer = (1000.0 / distance) * (System.Environment.TickCount - _previousPositionChangeTick);
+
+                // compute total distance travelled
+                _kilometres += distance / 1000.0;
+
+                paceLabel.Text = TimeSpan.FromMilliseconds(millisPerKilometer).ToString(@"mm\:ss");
+                distanceLabel.Text = string.Format("{0:f2} km", _kilometres);
+                caloriesLabel.Text = string.Format("{0:f0}", _kilometres * 65);
+            }
+
+
+            Map.Center = coord;
+
+            _line.Path.Add(coord);
+            _previousPositionChangeTick = System.Environment.TickCount;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan runTime = TimeSpan.FromMilliseconds(System.Environment.TickCount - _startTime);
+            timeLabel.Text = runTime.ToString(@"hh\:mm\:ss");
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_timer.IsEnabled)
+            {
+                _timer.Stop();
+                StartButton.Content = "Start";
+            }
+            else
+            {
+                _timer.Start();
+                _startTime = System.Environment.TickCount;
+                StartButton.Content = "Stop";
+            }
         }
     }
 }
